d0772509a4e85c6c884b2b420c835144b0077fd4
unknown <bebiashvili.dachi@gmail.com>
2026-03-15 17:59:47 +0400

    davalebaaaa38

--- products/serializers.py
+++ products/serializers.py
@@ -1,19 +1,5 @@
 from rest_framework import serializers
-from .models import Review, Product, FavoriteProduct, Cart
-
-# class ProductSerializer(serializers.Serializer):
-#     name = serializers.CharField()
-#     description = serializers.CharField()
-#     price = serializers.FloatField()
-#     currency = serializers.ChoiceField(choices=['GEL', 'USD', 'EUR'])
-
-
-# class ProductSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         # fields = "__all__"
-#         exclude = ['uuid', 'created_at', 'updated_at']
-#         model = Product
-
+from .models import Review, Product, ProductTag
 
 
 class ReviewSerializer(serializers.Serializer):
@@ -46,43 +32,6 @@ class ReviewSerializer(serializers.Serializer):
         return review
     
 
-                  
-       
-class FavoriteProductSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    product_id = serializers.IntegerField(write_only=True)
-
-    class Meta:
-        model = FavoriteProduct
-        fields = ['id', 'user', 'product_id', 'product']
-        read_only_fields = ['id', 'product']
-
-    def validate_product_id(self, value):
-        if not Product.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Invalid product_id. Product does not exist.")
-        return value
-
-    def create(self, validated_data):
-        product_id = validated_data.pop('product_id')
-        user = validated_data.pop('user')
-
-        product = Product.objects.get(id=product_id)
-        favorite, created = FavoriteProduct.objects.get_or_create(
-            user=user,
-            product=product
-        )
-
-        if not created:
-            raise serializers.ValidationError("This product is already in favorites.")
-
-        return favorite
-
-
-
-
-
-
-from .models import ProductTag
 
 class ProductTagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -115,36 +64,3 @@ class ProductSerializer(serializers.ModelSerializer):
         if tags is not None:
             instance.tags.set(tags)
         return super().update(instance, validated_data)
-
-
-class CartSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    products = ProductSerializer(many=True, read_only=True)
-    product_ids = serializers.PrimaryKeyRelatedField(
-        source='products',
-        queryset=Product.objects.all(),
-        many=True,
-        # write_only=True
-    )
-
-    class Meta:
-        model = Cart
-        fields = ["user", 'product_ids', 'products']
-
-    def create(self, validated_data):
-        user = validated_data.pop('user')
-        products = validated_data.get('products')
-
-        cart, _ = Cart.objects.get_or_create(user=user)
-        cart.products.add(*products)
-
-        return 
-    
-
-
-from .models import ProductImage
-
-class ProductImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductImage
-        fields = ['id', 'image', 'product']
